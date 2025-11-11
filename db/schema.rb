83bb60378e0599b06fc7846a5b0f7b1db9707f92
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-11 16:47:44 +0530

    added description to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_11_111144) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_11_111350) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
   end
