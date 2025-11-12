8e3e4805dde83c4eb0a4bee8efa1fbcc645348c8
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-12 16:45:30 +0530

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 25 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
     has_many :articles
 end
