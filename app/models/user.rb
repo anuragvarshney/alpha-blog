625d27a462e7d720e8de6e2d26bb67e2ee9d7bab
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-12 18:56:00 +0530

    down case user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 25 }
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
     has_many :articles
+    before_save { self.email = email.downcase }
 end
