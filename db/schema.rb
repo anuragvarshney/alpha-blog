2b8160a7a5268267bc4eaf9bce10f2f2e86a8413
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-12 17:21:30 +0530

    added user and article association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_12_103324) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_12_112611) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +26,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_12_103324) do
     t.datetime "updated_at", null: false
     t.string "username"
   end
+
+  add_foreign_key "articles", "users"
 end
