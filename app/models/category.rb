a51e14952d8d8c80395a4bc90bf522f88c7499a4
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-13 21:51:46 +0530

    article categories relation done

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 200 }
+    has_many :article_categories
+    has_many :articles, through: :article_categories
 end
