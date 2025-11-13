2f18cef4bfdf7edf1b904ce7dfcfeac6657ff2ed
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-13 18:06:13 +0530

    created category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_13_101036) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_13_122036) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_13_101036) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
