2bec28dcd568dc93da84f273e47d7d2215f4de1f
Anurag Varshney <anuragvarshney0309@gmail.com>
2025-11-13 10:29:45 +0530

    added index call for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,43 +2,49 @@ class UsersController < ApplicationController
     before_action :set_user, only: [ :show, :update, :destroy ]
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  def show
-    render json: @user.as_json(only: [ :id, :username, :email ])
-  end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    def index
+      @users = User.all
+      render json: @users.as_json(except: [ :password_digest ])
     end
-  end
 
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    def show
+      render json: @user.as_json(only: [ :id, :username, :email ])
     end
-  end
 
-  def destroy
-    @user.destroy
-    head :no_content
-  end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
 
-  private
+    def update
+      if @user.update(user_params)
+        render json: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def destroy
+      @user.destroy
+      head :no_content
+    end
 
-  def record_not_found
-    render json: { error: "User not found" }, status: :not_found
-  end
+    private
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password,)
-  end
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def record_not_found
+      render json: { error: "User not found" }, status: :not_found
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password,)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :articles, only: [ :show, :index, :create, :update, :destroy ]
-  resources :users, only: [ :show, :create, :update, :destroy ]
+  resources :users, only: [ :index, :show, :create, :update, :destroy ]
 end
